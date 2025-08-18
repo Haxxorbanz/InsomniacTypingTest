a4e5a9118e311ff6dd60feebf3a02f4e9d637d50
Haxxorbanz <bc.nelson.capili@cvsu.edu.ph>
2025-08-18 16:24:23 +0800

    Make a CTRL + ENTER refresh when typing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -22,8 +22,8 @@ class App extends React.Component {
         super(props);
         this.state = DefaultState;
 
-        // 👇 create a ref for the typing textarea
-        this.inputRef = React.createRef();
+        this.inputRef = React.createRef(); // textarea ref
+        this.timer = null; // store interval
     }
 
     fetchNewParagraphFallback = () => {
@@ -34,10 +34,7 @@ class App extends React.Component {
 
         const selectedParagraphArray = data.split("");
         const testInfo = selectedParagraphArray.map((selectedLetter) => {
-            return {
-                testLetter: selectedLetter,
-                status: "notAttempted",
-            };
+            return { testLetter: selectedLetter, status: "notAttempted" };
         });
 
         this.setState({
@@ -47,38 +44,45 @@ class App extends React.Component {
         });
     };
 
-    fetchNewParagraph = () => {
-        fetch("http://metaphorpsum.com/paragraphs/1/9")
-            .then((response) => response.text())
-            .then((data) => {
-                const selectedParagraphArray = data.split("");
-                const testInfo = selectedParagraphArray.map(
-                    (selectedLetter) => {
-                        return {
-                            testLetter: selectedLetter,
-                            status: "notAttempted",
-                        };
-                    }
-                );
-
-                this.setState({
-                    ...DefaultState,
-                    selectedParagraph: data,
-                    testInfo,
-                });
-            });
-    };
-
     componentDidMount() {
         this.fetchNewParagraphFallback();
+
+        // ✅ global shortcuts
+        window.addEventListener("keydown", this.handleKeyDown);
     }
 
+    componentWillUnmount() {
+        if (this.timer) clearInterval(this.timer);
+        window.removeEventListener("keydown", this.handleKeyDown);
+    }
+
+    handleKeyDown = (e) => {
+        // Ctrl + Enter → reset anytime
+        if (e.ctrlKey && e.key === "Enter") {
+            e.preventDefault();
+            this.startAgain();
+        }
+
+        // After time is up → allow R or Enter
+        if (!this.state.timerStarted && this.state.timeRemaining === 0) {
+            if (e.key === "r" || e.key === "R" || e.key === "Enter") {
+                e.preventDefault();
+                this.startAgain();
+            }
+        }
+    };
+
     startAgain = () => {
+        if (this.timer) {
+            clearInterval(this.timer);
+            this.timer = null;
+        }
+
         this.fetchNewParagraphFallback();
 
-        // 👇 focus the typing box after resetting
         setTimeout(() => {
             if (this.inputRef.current) {
+                this.inputRef.current.value = "";
                 this.inputRef.current.focus();
             }
         }, 0);
@@ -86,20 +90,24 @@ class App extends React.Component {
 
     startTimer = () => {
         this.setState({ timerStarted: true });
-        const timer = setInterval(() => {
-            if (this.state.timeRemaining > 0) {
-                const timeSpent = TotalTime - this.state.timeRemaining;
-                const wpm =
-                    timeSpent > 0
-                        ? (this.state.words / timeSpent) * TotalTime
-                        : 0;
-                this.setState({
-                    timeRemaining: this.state.timeRemaining - 1,
-                    wpm: parseInt(wpm),
-                });
-            } else {
-                clearInterval(timer);
-            }
+        this.timer = setInterval(() => {
+            this.setState((prevState) => {
+                if (prevState.timeRemaining > 0) {
+                    const timeSpent = TotalTime - prevState.timeRemaining;
+                    const wpm =
+                        timeSpent > 0
+                            ? (prevState.words / timeSpent) * TotalTime
+                            : 0;
+                    return {
+                        timeRemaining: prevState.timeRemaining - 1,
+                        wpm: parseInt(wpm),
+                    };
+                } else {
+                    clearInterval(this.timer);
+                    this.timer = null;
+                    return { timerStarted: false };
+                }
+            });
         }, 1000);
     };
 
@@ -107,7 +115,7 @@ class App extends React.Component {
         if (!this.state.timerStarted) this.startTimer();
 
         const characters = inputValue.length;
-        const words = inputValue.split(" ").length;
+        const words = inputValue.trim().split(" ").filter(Boolean).length;
         const index = characters - 1;
 
         if (index < 0) {
@@ -130,12 +138,12 @@ class App extends React.Component {
             return;
         }
 
-        const testInfo = this.state.testInfo;
+        const testInfo = [...this.state.testInfo];
         if (!(index === this.state.selectedParagraph.length - 1))
             testInfo[index + 1].status = "notAttempted";
 
-        const isMistake = inputValue[index] === testInfo[index].testLetter;
-        testInfo[index].status = isMistake ? "correct" : "incorrect";
+        const isCorrect = inputValue[index] === testInfo[index].testLetter;
+        testInfo[index].status = isCorrect ? "correct" : "incorrect";
 
         this.setState({ testInfo, words, characters });
     };
@@ -154,8 +162,8 @@ class App extends React.Component {
                     wpm={this.state.wpm}
                     timeRemaining={this.state.timeRemaining}
                     timerStarted={this.state.timerStarted}
-                    startAgain={this.startAgain}
-                    inputRef={this.inputRef}   // 👈 pass ref down
+                    startAgain={this.startAgain} // button reset
+                    inputRef={this.inputRef}     // typing box ref
                 />
                 <Footer />
             </div>
